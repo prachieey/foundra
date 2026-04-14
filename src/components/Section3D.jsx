10e407e52e98b098c7ce0a320284e889e44be5af
Sehgalnikhil <nikhilsehgal1806@gmail.com>
2026-04-14 17:41:46 +0530

    perf: optimized 3d scroll and spotlight effects for buttery smooth 60fps

--- src/components/Section3D.jsx
+++ src/components/Section3D.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion, useScroll, useTransform, useSpring } from 'framer-motion';
 
 const Section3D = ({ children, className }) => {
   const ref = React.useRef(null);
@@ -8,20 +8,30 @@ const Section3D = ({ children, className }) => {
     offset: ["start end", "end start"]
   });
 
-  const rotateX = useTransform(scrollYProgress, [0, 0.5, 1], [15, 0, -15]);
-  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.9, 1, 0.9]);
-  const opacity = useTransform(scrollYProgress, [0, 0.2, 0.8, 1], [0, 1, 1, 0]);
-  const z = useTransform(scrollYProgress, [0, 0.5, 1], [-100, 0, -100]);
+  // Use springs to smooth out calculations and reduce jitter/lag
+  const springConfig = { stiffness: 100, damping: 30, restDelta: 0.001 };
+  
+  const rawRotateX = useTransform(scrollYProgress, [0, 0.5, 1], [10, 0, -10]);
+  const rawScale = useTransform(scrollYProgress, [0, 0.5, 1], [0.95, 1, 0.95]);
+  const rawOpacity = useTransform(scrollYProgress, [0, 0.15, 0.85, 1], [0, 1, 1, 0]);
+  const rawZ = useTransform(scrollYProgress, [0, 0.5, 1], [-50, 0, -50]);
+
+  const rotateX = useSpring(rawRotateX, springConfig);
+  const scale = useSpring(rawScale, springConfig);
+  const opacity = useSpring(rawOpacity, springConfig);
+  const z = useSpring(rawZ, springConfig);
 
   return (
     <motion.div
       ref={ref}
       style={{
-        perspective: "1000px",
+        perspective: "1200px",
         rotateX,
         scale,
         opacity,
         z,
+        willChange: "transform, opacity",
+        transformStyle: "preserve-3d"
       }}
       className={className}
     >
